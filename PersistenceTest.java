ee852fb96ed07d9065a6563797d05ad0920dd507
Mateusz Gryczan <44230456+arrlong97@users.noreply.github.com>
2020-07-27 15:40:16 +0200

    Create PersistenceTest.java

---
+++ PersistenceTest.java
@@ -0,0 +1,15 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateTest {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        System.out.println("DONE!");
+    }
+}
